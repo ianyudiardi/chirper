054bbda3f5e5ce3e638c134a693197d2a603e8ea
ianyudiardi <ianyudiardi@gmail.com>
2026-07-04 20:03:32 +0700

    Implement chirp editing and deletion functionality with authorization checks

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
 
 Route::get('/', [ChirpController::class, 'index']);
-Route::post('/chirps', [ChirpController::class, 'store']);
+
+Route::resource('chirps', ChirpController::class)->only([
+    'store', 'edit', 'update', 'destroy'
+]);
+
+// the route above is equivalent to the following routes:
+
+// Route::post('/chirps', [ChirpController::class, 'store']);
+// Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+// Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+// Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
